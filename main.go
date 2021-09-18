cd811b1371ac73524726306050446092e0137a53
Khoa Le <ltkhoa2711@gmail.com>
2021-09-18 11:21:09 +1000

    refactor: reorganize the pkg folder structure
    to prepare for adding other packages aside elasticsearch, such as s3

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
-	elasticsearch "github.com/tkhoa2711/aws-best-practices-checker/pkg"
+	elasticsearch "github.com/tkhoa2711/aws-best-practices-checker/pkg/elasticsearch"
+)
+
+var (
+	ignoreNonProdEnv bool
 )
 
 func main() {
+	flag.BoolVar(&ignoreNonProdEnv, "ignore-non-prod", true, "Whether to ignore non-production environments, such as dev and staging")
+
 	elasticsearchDomains, _ := elasticsearch.GetAllElasticsearchDomains()
 	for _, domainName := range elasticsearchDomains {
 		err := elasticsearch.CheckInstanceType(&domainName)
+		if err != nil {
+			fmt.Println(err)
+		}
+
 		err = elasticsearch.CheckDedicatedMasterNodes(&domainName)
 
 		if err != nil {
